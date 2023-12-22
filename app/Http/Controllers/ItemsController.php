18a67617a100f407a61a8335fb4899aa42202c00
Christian Morales <chrisrodo84@gmail.com>
2023-12-22 10:43:42 -0600

    Updating ítems response to return more details

--- app/Http/Controllers/ItemsController.php
+++ app/Http/Controllers/ItemsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Items;
+use App\Models\Tareas;
 
 class ItemsController extends Controller
 {
@@ -11,7 +12,7 @@ class ItemsController extends Controller
             "status" => "400",
             "detalle" => "Solicitud incorrecta, proporcione el id de una cotización"
         );
-        
+
         return json_encode($json, true);
     }
 
@@ -24,76 +25,7 @@ class ItemsController extends Controller
         ->get();
 
         if (count($items) > 0) {
-            $arrItems = json_decode($items, true); 
-
-            // Se calcula las horas por área de forma global
-            $horasAM = 0;
-            $horasAU = 0;
-            $horasCM = 0;
-            $horasCR = 0;
-            $horasDD = 0;
-            $horasDE = 0;
-            $horasED = 0;
-            $horasPR = 0;
-
-            foreach ($arrItems as $key => $item) {
-                $am = explode(',', $item["horas_am"]);
-                $au = explode(',', $item["horas_au"]);
-                $cm = explode(',', $item["horas_cm"]);
-                $cr = explode(',', $item["horas_cr"]);
-                $dd = explode(',', $item["horas_dd"]);
-                $de = explode(',', $item["horas_de"]);
-                $ed = explode(',', $item["horas_ed"]);
-                $pr = explode(',', $item["horas_pr"]);
-
-                for ($i=0; $i < count($am); $i++) { 
-                    if ($am[$i] !== '0') {
-                        $horasAM = $horasAM + intval($am[$i]);
-                    }
-                }
-
-                for ($i=0; $i < count($au); $i++) { 
-                    if ($au[$i] !== '0') {
-                        $horasAU = $horasAU + intval($au[$i]);
-                    }
-                }
-                
-                for ($i=0; $i < count($cm); $i++) { 
-                    if ($cm[$i] !== '0') {
-                        $horasCM = $horasCM + intval($cm[$i]);
-                    }
-                }
-                
-                for ($i=0; $i < count($cr); $i++) { 
-                    if ($cr[$i] !== '0') {
-                        $horasCR = $horasCR + intval($cr[$i]);
-                    }
-                }
-                
-                for ($i=0; $i < count($dd); $i++) { 
-                    if ($dd[$i] !== '0') {
-                        $horasDD = $horasDD + intval($dd[$i]);
-                    }
-                }
-                
-                for ($i=0; $i < count($de); $i++) { 
-                    if ($de[$i] !== '0') {
-                        $horasDE = $horasDE + intval($de[$i]);
-                    }
-                }
-                
-                for ($i=0; $i < count($ed); $i++) { 
-                    if ($ed[$i] !== '0') {
-                        $horasED = $horasED + intval($ed[$i]);
-                    }
-                }
-                
-                for ($i=0; $i < count($pr); $i++) { 
-                    if ($pr[$i] !== '0') {
-                        $horasPR = $horasPR + intval($pr[$i]);
-                    }
-                }
-            }
+            $arrItems = json_decode($items, true);
 
             foreach ($arrItems as $key => $item) {
                 $totalHorasAM = 0;
@@ -105,6 +37,15 @@ class ItemsController extends Controller
                 $totalHorasED = 0;
                 $totalHorasPR = 0;
 
+                $tareasAM = array();
+                $tareasAU = array();
+                $tareasCR = array();
+                $tareasCM = array();
+                $tareasDD = array();
+                $tareasDE = array();
+                $tareasED = array();
+                $tareasPR = array();
+
                 $am = explode(',', $item["horas_am"]);
                 $au = explode(',', $item["horas_au"]);
                 $cm = explode(',', $item["horas_cm"]);
@@ -114,70 +55,215 @@ class ItemsController extends Controller
                 $ed = explode(',', $item["horas_ed"]);
                 $pr = explode(',', $item["horas_pr"]);
 
-                for ($i=0; $i < count($am); $i++) { 
+                for ($i=0; $i < count($am); $i++) {
                     if ($am[$i] !== '0') {
                         $totalHorasAM = $totalHorasAM + intval($am[$i]);
+
+                        $tarea = Tareas::select()
+                            ->where('id_custom', 'am_t'.($i+1))
+                        ->get();
+
+                        $arrTarea = json_decode($tarea, true);
+
+                        $detalle_tarea = array(
+                            "nombre_tarea" => $arrTarea[0]["descripcion_tarea"],
+                            "horas_tarea" => intval($am[$i])
+                        );
+
+                        array_push($tareasAM, $detalle_tarea);
                     }
                 }
 
-                for ($i=0; $i < count($au); $i++) { 
+                for ($i=0; $i < count($au); $i++) {
                     if ($au[$i] !== '0') {
                         $totalHorasAU = $totalHorasAU + intval($au[$i]);
+
+                        $tarea = Tareas::select()
+                            ->where('id_custom', 'au_t'.($i+1))
+                        ->get();
+
+                        $arrTarea = json_decode($tarea, true);
+
+                        $detalle_tarea = array(
+                            "nombre_tarea" => $arrTarea[0]["descripcion_tarea"],
+                            "horas_tarea" => intval($au[$i])
+                        );
+
+                        array_push($tareasAU, $detalle_tarea);
                     }
                 }
-                
-                for ($i=0; $i < count($cm); $i++) { 
+
+                for ($i=0; $i < count($cm); $i++) {
                     if ($cm[$i] !== '0') {
                         $totalHorasCM = $totalHorasCM + intval($cm[$i]);
+
+                        $tarea = Tareas::select()
+                            ->where('id_custom', 'cm_t'.($i+1))
+                        ->get();
+
+                        $arrTarea = json_decode($tarea, true);
+
+                        $detalle_tarea = array(
+                            "nombre_tarea" => $arrTarea[0]["descripcion_tarea"],
+                            "horas_tarea" => intval($cm[$i])
+                        );
+
+                        array_push($tareasCM, $detalle_tarea);
                     }
                 }
-                
-                for ($i=0; $i < count($cr); $i++) { 
+
+                for ($i=0; $i < count($cr); $i++) {
                     if ($cr[$i] !== '0') {
                         $totalHorasCR = $totalHorasCR + intval($cr[$i]);
+
+                        $tarea = Tareas::select()
+                            ->where('id_custom', 'cr_t'.($i+1))
+                        ->get();
+
+                        $arrTarea = json_decode($tarea, true);
+
+                        $detalle_tarea = array(
+                            "nombre_tarea" => $arrTarea[0]["descripcion_tarea"],
+                            "horas_tarea" => intval($cr[$i])
+                        );
+
+                        array_push($tareasCR, $detalle_tarea);
                     }
                 }
-                
-                for ($i=0; $i < count($dd); $i++) { 
+
+                for ($i=0; $i < count($dd); $i++) {
                     if ($dd[$i] !== '0') {
                         $totalHorasDD = $totalHorasDD + intval($dd[$i]);
+
+                        $tarea = Tareas::select()
+                            ->where('id_custom', 'dd_t'.($i+1))
+                        ->get();
+
+                        $arrTarea = json_decode($tarea, true);
+
+                        $detalle_tarea = array(
+                            "nombre_tarea" => $arrTarea[0]["descripcion_tarea"],
+                            "horas_tarea" => intval($dd[$i])
+                        );
+
+                        array_push($tareasDD, $detalle_tarea);
                     }
                 }
-                
-                for ($i=0; $i < count($de); $i++) { 
+
+                for ($i=0; $i < count($de); $i++) {
                     if ($de[$i] !== '0') {
                         $totalHorasDE = $totalHorasDE + intval($de[$i]);
+
+                        $tarea = Tareas::select()
+                            ->where('id_custom', 'de_t'.($i+1))
+                        ->get();
+
+                        $arrTarea = json_decode($tarea, true);
+
+                        $detalle_tarea = array(
+                            "nombre_tarea" => $arrTarea[0]["descripcion_tarea"],
+                            "horas_tarea" => intval($de[$i])
+                        );
+
+                        array_push($tareasDE, $detalle_tarea);
                     }
                 }
-                
-                for ($i=0; $i < count($ed); $i++) { 
+
+                for ($i=0; $i < count($ed); $i++) {
                     if ($ed[$i] !== '0') {
                         $totalHorasED = $totalHorasED + intval($ed[$i]);
+
+                        $tarea = Tareas::select()
+                            ->where('id_custom', 'ed_t'.($i+1))
+                        ->get();
+
+                        $arrTarea = json_decode($tarea, true);
+
+                        $detalle_tarea = array(
+                            "nombre_tarea" => $arrTarea[0]["descripcion_tarea"],
+                            "horas_tarea" => intval($ed[$i])
+                        );
+
+                        array_push($tareasED, $detalle_tarea);
                     }
                 }
-                
-                for ($i=0; $i < count($pr); $i++) { 
+
+                for ($i=0; $i < count($pr); $i++) {
                     if ($pr[$i] !== '0') {
                         $totalHorasPR = $totalHorasPR + intval($pr[$i]);
+
+                        $tarea = Tareas::select()
+                            ->where('id_custom', 'pr_t'.($i+1))
+                        ->get();
+
+                        $arrTarea = json_decode($tarea, true);
+
+                        $detalle_tarea = array(
+                            "nombre_tarea" => $arrTarea[0]["descripcion_tarea"],
+                            "horas_tarea" => intval($pr[$i])
+                        );
+
+                        array_push($tareasPR, $detalle_tarea);
                     }
                 }
-                
-                $arrHorasArea = array(
-                    "horas_area_medica" => $totalHorasAM,
-                    "horas_audiovisual" => $totalHorasAU,
-                    "horas_comercial" => $totalHorasCM,
-                    "horas_creativo" => $totalHorasCR,
-                    "horas_diseno_digital" => $totalHorasDD,
-                    "horas_diseno_editorial" => $totalHorasDE,
-                    "horas_editorial" => $totalHorasED,
-                    "horas_programacion" => $totalHorasPR,
+
+                $area_medica = array(
+                    "total_horas" => $totalHorasAM,
+                    "tareas_item" => $tareasAM
                 );
-                
+
+                $audiovisual = array(
+                    "total_horas" => $totalHorasAU,
+                    "tareas_item" => $tareasAU
+                );
+
+                $comercial = array(
+                    "total_horas" => $totalHorasCM,
+                    "tareas_item" => $tareasCM
+                );
+
+                $creativo = array(
+                    "total_horas" => $totalHorasCR,
+                    "tareas_item" => $tareasCR
+                );
+
+                $diseno_digital = array(
+                    "total_horas" => $totalHorasDD,
+                    "tareas_item" => $tareasDD
+                );
+
+                $diseno_editorial = array(
+                    "total_horas" => $totalHorasDE,
+                    "tareas_item" => $tareasDE
+                );
+
+                $editorial = array(
+                    "total_horas" => $totalHorasED,
+                    "tareas_item" => $tareasED
+                );
+
+                $programacion = array(
+                    "total_horas" => $totalHorasPR,
+                    "tareas_item" => $tareasPR
+                );
+
+                $arrHorasArea = array(
+                    "area_medica" => $area_medica,
+                    "audiovisual" => $audiovisual,
+                    "comercial" => $comercial,
+                    "creativo" => $creativo,
+                    "diseno_digital" => $diseno_digital,
+                    "diseno_editorial" => $diseno_editorial,
+                    "editorial" => $editorial,
+                    "programacion" => $programacion,
+                );
+
                 // Se crea el arreglo para el JSON
                 $newArrItem = array(
                     // "total_horas_area" => $arrTotalHorasArea,
+                    "id_item" => $item["id_items"],
                     "nombre_item" => $item["nombre_item"],
-                    "total_horas_item" => $arrHorasArea
+                    "detalle_item" => $arrHorasArea
                 );
                 array_push($arrGralItems, $newArrItem);
             }
@@ -186,14 +272,14 @@ class ItemsController extends Controller
                 "status" => "200",
                 "detalle" => $arrGralItems
             );
-            
+
             return json_encode($json, true);
         } else {
             $json = array(
                 "status" => "200",
                 "detalle" => "No hay ítems en la cotización proporcionada"
             );
-            
+
             return json_encode($json, true);
         }
     }
